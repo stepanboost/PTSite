e73454d45f0d5ed0656d90a83b37a39bd45ff573
Степан <stepan@MacBook-Pro-Stepik.local>
2025-12-18 18:35:15 +0300

    Fix Netlify config for Next.js plugin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     domains: ['images.unsplash.com'],
-    unoptimized: true,
   },
 }
 
